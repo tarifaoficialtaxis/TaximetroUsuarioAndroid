afa9c7fc40342fa2a1cd69794b9160f762312c3c
tarifaoficialtaxis <tarifaoficialtaxis@gmail.com>
2025-05-18 20:24:38 -0400

    General updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         applicationId = "com.mitarifamitaxi.taximetrousuario"
         minSdk = 29
         targetSdk = 35
-        versionCode = 8
+        versionCode = 9
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
